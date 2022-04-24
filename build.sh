738499d5ef017cc117229819b9ddd1ff1b9fba83
Joseph Johansen <joe@stotles.com>
2022-04-24 21:34:16 +0100

    Add 48px img to chrome

--- build.sh
+++ build.sh
@@ -4,10 +4,12 @@
 cp index.css chrome/boringstagram.css
 cp index.css firefox/boringstagram.css
 
-cp b.16.png chrome/b.16.png
+cp b.16.png chrome/b16.png
 cp b.16.png firefox/b.16.png
 
-cp b.128.png chrome/b.128.png
+cp b.48.png chrome/b48.png
+
+cp b.128.png chrome/b128.png
 cp b.128.png firefox/b.128.png
 
 cd firefox/ || exit
