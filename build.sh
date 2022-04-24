7ce4300e3345198887729a7eb3f12eeb141fe978
Joseph Johansen <joe@stotles.com>
2022-04-24 18:35:15 +0100

    Use properly sized images

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@
 cp index.css chrome/boringstagram.css
 cp index.css firefox/boringstagram.css
 
+cp b.16.png chrome/b.16.png
+cp b.16.png firefox/b.16.png
+
+cp b.128.png chrome/b.128.png
+cp b.128.png firefox/b.128.png
+
 cd firefox/ || exit
 zip -r -FS ../firefox.zip ./*
 cd .. || exit
