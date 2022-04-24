466465c1e5d0cce708b7f675cb8722c3240f80ec
Joseph Johansen <joe@stotles.com>
2022-04-24 18:30:25 +0100

    Fix build script for firefox

--- build.sh
+++ build.sh
@@ -4,5 +4,10 @@
 cp index.css chrome/boringstagram.css
 cp index.css firefox/boringstagram.css
 
-zip -r firefox.zip firefox/
-zip -r chrome.zip chrome/
+cd firefox/ || exit
+zip -r -FS ../firefox.zip ./*
+cd .. || exit
+
+cd chrome/ || exit
+zip -r -FS ../chrome.zip ./*
+cd .. || exit
